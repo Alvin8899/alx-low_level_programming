df032dd90d7bd839b4bca24de2f5b8c243200b71
Chimezie Gideon Nwaeze <chimeziegideon@gmail.com>
2022-09-09 15:57:03 -0700

    sw

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,25 +13,3 @@ int main(void)
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
-
-Task 7
-Vi 100-intel
-
-#!/bin/bash
-gcc -S -masm=intel $CFILE
-
-Task 8
-Vi 101-quote.c
-
-#include <unistd.h>
-/**
- *main-program entry point.
- *
- *Return: 1 on success.
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
-  
